23dc01922fdce28557437ea4dc7562d2afd795c2
Hongyiel Suh <suhho@oregonstate.edu>
2021-01-20 16:27:00 +0900

    Done
    WebScrapper

--- scrapper_main.py
+++ scrapper_main.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template,request,redirect
+from flask import Flask, render_template,request,redirect,send_file
 from scrapper import get_jobs
+from save import save_to_file
+
 
 app = Flask("SuperScrapper")
 
@@ -11,22 +13,40 @@ def home():
 
 @app.route("/report")
 def report():
-
-    # print(request.args.get('word'))
     word = request.args.get('word')
     if word:
         word = word.lower()
-        jobs = get_jobs(word)
-        print(jobs)
-        # fromDb = db.get(word)
-        # if fromDb:
-        #     jobs = fromDb
-        # else:
-        #     jobs = get_jobs(word)
-        #     db[word] = jobs
+        existingJobs = db.get(word)
+        if existingJobs:
+            jobs = existingJobs
+        else:
+            jobs = get_jobs(word)
+            db[word] = jobs
     else:
         return redirect("/")
-    return render_template("report.html", searchingBy=word)
+    return render_template("report.html",
+                            resultsNumber=len(jobs),
+                            searchingBy=word,
+                            jobs=jobs
+                            )
+
+
+@app.route("/export")
+def export():
+    try:
+        word = request.args.get('word')
+        if not word:
+            raise Exception()
+        word = word.lower()
+        jobs = db.get(word)
+        if not jobs:
+            raise Exception()
+        save_to_file(jobs)
+        
+        return send_file("jobs.csv")
+    except:
+        return redirect("/")
+
 app.run()
 
 # app.run(host="0.0.0.0")
